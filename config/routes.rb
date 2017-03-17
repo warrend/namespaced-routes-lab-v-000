fff1e70df5e5237c5286e8750dbe64b453e3566d
warrend <warrend555@gmail.com>
2017-03-17 14:27:39 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do 
+  	resources :preferences, only: [:index]
+  end
+
   resources :artists do
     resources :songs, only: [:index, :show]
   end
